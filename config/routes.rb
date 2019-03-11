e0a5214b674bc3eac7d9f9a6dbe69c115858fc87
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-03-11 19:27:56 +0100

    Created Session controller and actions new, create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
   get 'signup'  => 'users#new' 
   get 'attending'  => 'users#show'
