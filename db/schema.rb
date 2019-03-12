a7d2f8af4962100be9fe3850c79dbb9b6582cbf8
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-03-12 14:19:24 +0100

    Created event model; Migrated event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_093900) do
+ActiveRecord::Schema.define(version: 2019_03_12_133539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.string "location"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2019_03_12_093900) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "users"
 end
