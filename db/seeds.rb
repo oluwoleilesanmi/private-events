957930f601257b9991d436c874c9c37804123ff1
Oluwole Ilesanmi <oluwoleilesanmi@gmail.com>
2019-03-22 21:51:25 +0100

    Added auto setup code to seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+carlos = User.create(name: 'Carlos')
+freida = User.create(name: 'Frieda')
+julian = User.create(name: 'Julian')
+
+
+eventa = Event.create(name: "bash", date:'2001-01-10' , location: 'lagos', user_id: carlos.id)
+eventb = Event.create(name: "bashsecond", date:'2001-01-10' , location:'lagos', user_id: carlos.id)
+eventc = Event.create(name: "bashthird", date:'2001-01-10', location:'lagos', user_id: carlos.id)
+
+
+Invitation.create(user_id: freida.id, event_id: eventa.id, attend: true, invite: true)
+Invitation.create(user_id: freida.id, event_id: eventb.id, attend: true, invite: true)
+Invitation.create(user_id: freida.id, event_id: eventc.id, attend: true, invite: true)
+
+Invitation.create(user_id: julian.id, event_id: eventa.id, attend: true, invite: true)
+Invitation.create(user_id: julian.id, event_id: eventb.id, attend: true, invite: true)
+Invitation.create(user_id: julian.id, event_id: eventc.id, attend: true, invite: true)
